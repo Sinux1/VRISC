abb9204f1c553cdddaddbc00a007e86f22690bf5
sinux1 <sinux1@programmer.net>
2019-10-11 18:49:14 -0700

    Added much of control unit functionality in the fetch, decode, and execute methods.

--- src/ControlUnit.java
+++ src/ControlUnit.java
@@ -1,7 +1,7 @@
 public class ControlUnit {
 
 
-    // Variables
+    // Variables defined in instructions
     public boolean isStopped = false;
     private byte instructionRegister = 0;
     private byte programCounter = 0;
@@ -10,31 +10,99 @@ public class ControlUnit {
     private byte accumulator = 0;
     private byte operandRegister = 0;
     private boolean isDirect = false;
-    private RAM memory = null;
+    private RAM ram = null;
     private ALU alu = null;
+    // Magic unicorn food
+    private byte dataRegister1 = 0;
+    final boolean READ = false;
+    final boolean WRITE = !READ;
 
     // Constructor
     public ControlUnit(){
         ALU alu = new ALU();
+        RAM ram = new RAM();
 
     }
 
     // Methods
 
     private void fetchInstruction(){
+        // Place address in PC into MBR
         memoryAddressRegister = programCounter;
+
+        // Place value at mar into mbr from ram
+        ram.setMode(READ);
+        memoryBufferRegister = ram.readByte(memoryAddressRegister);
+
+        // Increment PC
+        programCounter++;
+
+        // Place value of mbr into ir
+        instructionRegister = memoryBufferRegister;
 
     }
 
     private void decodeInstruction(){
 
+
+        // Update isDirect - instructions with binary values less that 8 and are odd
+        // are instructions in immediate mode, else they are in direct mode
+        isDirect = (instructionRegister < 8) && ((instructionRegister % 2) == 1);
+
+        // If a binary instruction, fetch operand
+        if((instructionRegister > 0 && instructionRegister < 12)){
+            // Place address in PC into MBR
+            memoryAddressRegister = programCounter;
+
+            // Place value at mar into mbr from ram
+            ram.setMode(READ);
+            memoryBufferRegister = ram.readByte(memoryAddressRegister);
+
+            // Increment PC
+            programCounter++;
+
+            memoryBufferRegister = dataRegister1;
+
+            if(isDirect) {
+                fetchOperand();
+            }
+
+            operandRegister = dataRegister1;
+        }
+
+
+
+
+
     }
 
     private void fetchOperand(){
+        memoryAddressRegister = dataRegister1;
+        ram.setMode(READ);
+        memoryBufferRegister = ram.readByte(memoryAddressRegister);
+        dataRegister1 = memoryBufferRegister;
 
     }
 
     private void execute(){
+        if(instructionRegister == 0){
+            stop();
+        } else if (instructionRegister == 2 || instructionRegister == 3) {
+            load();
+        } else if (instructionRegister == 4 || instructionRegister == 5){
+            store();
+        } else if (instructionRegister == 6 || instructionRegister == 7){
+            compare();
+        }else if (instructionRegister == 8){
+            jump();
+        }else if (instructionRegister == 9){
+            jumpIfEqual();
+        }else if (instructionRegister == 10){
+            jumpIfLessThan();
+        }else if (instructionRegister == 11){
+            jumpIfGreaterThan();
+        }
+
 
     }
 
@@ -82,6 +150,12 @@ public class ControlUnit {
 
     public static void main(String[] args){
         System.out.println("WTF, crae crae");
+        ControlUnit cu = new ControlUnit();
+        while(!(cu.isStopped)) {
+
+        }
+
+
     }
 }
 
--- src/RAM.java
+++ src/RAM.java
@@ -3,27 +3,29 @@ import jdk.dynalink.linker.MethodTypeConversionStrategy;
 public class RAM{
 
     private boolean isWriteMode = false;
-    private byte[] memory = {2,7,7,64,9,20,10,25,2,3,4,70,0,0,0,0,0,0,0,0,2,1,4,70,0,2,2,4,70,0,0,0,0,0,0,0,0,0,0,0,0,
+    public byte[] memory = {2,7,7,64,9,20,10,25,2,3,4,70,0,0,0,0,0,0,0,0,2,1,4,70,0,2,2,4,70,0,0,0,0,0,0,0,0,0,0,0,0,
                                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,85,0,0,0,0,0,0,0,0,0,0,0,0,0,
                                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                                 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-    public RAM(){
-
-
-    }
 
     public void setMode(boolean mode){
         isWriteMode = mode;
     }
 
     public byte readByte(byte address){
-        return address;
+        return memory[address];
+
     }
 
     public void writeByte(byte address, byte value){
+
+        if(isWriteMode){
+            memory[address] = value;
+        }
+        setMode((false));
 
     }
 
